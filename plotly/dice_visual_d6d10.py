03f8e5d2c007e979412ad9f22143333e0f0bfdcc
Nathan <nslee333@gmail.com>
2023-03-31 23:22:44 -0700

    Added a write_html and corresponding html from plotly.

--- plotly/dice_visual_d6d10.py
+++ plotly/dice_visual_d6d10.py
@@ -5,4 +5,37 @@ from die import Die
 die_1 = Die(6)
 die_2 = Die(10)
 
+# Make some rolls, and store the results in a list
+results = []
+for roll_num in range(50_000):
+    result = die_1.roll() + die_2.roll()
+    results.append(result)
 
+# Analyze the results.
+frequencies = [] 
+max_results = die_1.num_sides + die_2.num_sides
+poss_results = range(2, max_results + 1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Visualize the results
+title = "Results of Rolling a D6 and a D10 50,000 times."
+labels = {
+    'x': 'Result', 
+    'y': 'Frequency of Result'
+}
+
+fig = px.bar(
+    x=poss_results, 
+    y=frequencies,
+    title=title,
+    labels=labels
+    )
+
+# Further customize chart.
+fig.update_layout(xaxis_dtick=1)
+
+# fig.show()
+fig.write_html('dice_visual_d6d10.html')
+    
