c95345af7d29c425e10b3cfee2c67a4222e2b9a6
Nathan <nslee333@gmail.com>
2023-03-31 18:34:34 -0700

    Added coloring to walk

--- random_walk/rw_visual.py
+++ random_walk/rw_visual.py
@@ -11,10 +11,12 @@ while True:
     # Plot the points in the walk.
     plt.style.use('classic')
     fig, ax = plt.subplots()
-
-    ax.scatter(rw.x_values, rw.y_values, s=15)
+    
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+    
     ax.set_aspect('equal')
-
+    
     plt.show()
     
     keep_running = input("Make another walk? (y/n): ")
