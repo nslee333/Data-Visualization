9a17bb42bd2158eac6fdd2f89be0ab9e58ee9588
Nathan <nslee333@gmail.com>
2023-03-31 18:28:53 -0700

    Added program looping with user input.

--- random_walk/rw_visual.py
+++ random_walk/rw_visual.py
@@ -2,14 +2,21 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk 
 
-rw = RandomWalk()
-rw.fill_walk()
+# Keep making new walks, as long as the program is active.
+while True:
 
-# Plot the points in the walk.
-plt.style.use('classic')
-fig, ax = plt.subplots()
+    rw = RandomWalk()
+    rw.fill_walk()
 
-ax.scatter(rw.x_values, rw.y_values, s=15)
-ax.set_aspect('equal')
+    # Plot the points in the walk.
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
 
-plt.show()
+    ax.scatter(rw.x_values, rw.y_values, s=15)
+    ax.set_aspect('equal')
+
+    plt.show()
+    
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
