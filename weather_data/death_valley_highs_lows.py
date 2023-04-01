1f0e3ee2b0a86340d04ac5150f1e440dd0595ecf
Nathan <nslee333@gmail.com>
2023-04-01 10:17:29 -0700

    Graph visualization file that handles errors in the dataset.

---
+++ weather_data/death_valley_highs_lows.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+import csv
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+path = Path('weather_csv/death_valley_2021_simple.csv')
+lines = path.read_text().splitlines()
+
+reader = csv.reader(lines)
+header_row = next(reader)
+
+
+dates, highs, lows = [], [], []
+for row in reader:
+    current_date = datetime.strptime(row[2], '%Y-%m-%d')
+    
+    try:
+        high = int(row[3])
+        low = int(row[4])
+    except ValueError:
+        print(f"Missing data for {current_date}")
+    else:
+        dates.append(current_date)
+        highs.append(high)
+        lows.append(low)
+
+# Plot the high temperatures.
+fig, ax = plt.subplots()
+ax.plot(dates, highs, color='red', alpha=0.5)
+ax.plot(dates, lows, color='blue', alpha=0.5)
+ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
+# Format plot.
+ax.set_title("Daily high and low temperatures, 2021", fontsize=20)
+ax.set_xlabel('', fontsize=14)
+fig.autofmt_xdate() 
+ax.set_ylabel("Temperature (F)", fontsize=14)
+ax.tick_params(labelsize=14)
+
+plt.show()
