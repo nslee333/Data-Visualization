0b483014abff55ae60cd2e314ae8ad23e920bd42
Nathan <nslee333@gmail.com>
2023-04-01 13:07:16 -0700

    Accesses earthquake data, then logs it.

--- eq_data/eq_explore_data.py
+++ eq_data/eq_explore_data.py
@@ -7,7 +7,19 @@ contents = path.read_text()
 all_eq_data = json.loads(contents)
 
 
-# Create a more readable version of the data file.
-path = Path('readable_eq_data.geojson')
-readable_contents = json.dumps(all_eq_data, indent=4)
-path.write_text(readable_contents)
+# Examine all earthquakes in the dataset.
+all_eq_dicts = all_eq_data['features']
+
+
+mags, lons, lats = [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+
+print(mags[:10])
+print(lons[:5])
+print(lats[:5])
