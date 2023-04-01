3dbc908913b35e806934b176721abf1e282aa250
Nathan <nslee333@gmail.com>
2023-04-01 15:15:26 -0700

    Added a hover feature to script.

--- eq_data/eq_world_map.py
+++ eq_data/eq_world_map.py
@@ -13,14 +13,17 @@ all_eq_data = json.loads(contents)
 all_eq_dicts = all_eq_data['features']
 
 
-mags, lons, lats = [], [], []
+mags, lons, lats, eq_titles = [], [], [], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
+    eq_title = eq_dict['properties']['title']
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+    eq_titles.append(eq_title)
+
     
 title = 'Global Earthquakes'
 color_red = 'red'
@@ -33,5 +36,7 @@ fig = px.scatter_geo(
     color_continuous_scale='Viridis',
     labels={'color':'Magnitude'},
     projection='natural earth',
+    hover_name=eq_titles
     )
+
 fig.show()
