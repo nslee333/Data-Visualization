c8ccc6fd449d477bebafa19c9124259309647a90
Nathan <nslee333@gmail.com>
2023-03-31 21:54:34 -0700

    Added two dice to the visualization.

--- plotly/die_visual.py
+++ plotly/die_visual.py
@@ -1,24 +1,26 @@
 from die import Die
 import plotly.express as px
 
-# Create a D6
-die = Die()
+# Create two D6 dies.
+die_1 = Die()
+die_2 = Die()
 
 # Make some rolls and store results in a list.
 results = []
 for roll_num in range(1000):
-    result = die.roll()
+    result = die_1.roll() + die_2.roll()
     results.append(result)
     
 # Analyze the results.
 frequencies = [] 
-poss_results = range(1, die.num_sides + 1)
+max_results = die_1.num_sides + die_2.num_sides
+poss_results = range(2, max_results + 1)
 for value in poss_results:
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # Visualize the results
-title = "Results of Rolling One D6 1,000 times."
+title = "Results of Rolling two D6 1,000 times."
 labels = {
     'x': 'Result', 
     'y': 'Frequency of Result'
@@ -30,4 +32,8 @@ fig = px.bar(
     title=title,
     labels=labels
     )
+
+# Further customize chart.
+fig.update_layout(xaxis_dtick=1)
+
 fig.show()
