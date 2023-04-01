8ec6b94771bf20754209ee7703cf91eede7fc473
Nathan <nslee333@gmail.com>
2023-04-01 09:46:38 -0700

    Added shading between the two plots.

--- weather_data/sitka_highs_lows.py
+++ weather_data/sitka_highs_lows.py
@@ -20,8 +20,9 @@ for row in reader:
 
 # Plot the high temperatures.
 fig, ax = plt.subplots()
-ax.plot(dates, highs, color='red')
-ax.plot(dates, lows, color='blue')
+ax.plot(dates, highs, color='red', alpha=0.5)
+ax.plot(dates, lows, color='blue', alpha=0.5)
+ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Format plot.
 ax.set_title("Daily high and temperatures, 2021", fontsize=20)
